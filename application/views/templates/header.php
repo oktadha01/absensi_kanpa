03ab0633650609bf00beba5c20943899ae7013d6
oktadha01 <oktadha01@gmail.com>
2023-10-06 18:13:52 +0700

    header

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -2,10 +2,10 @@
 <html lang="en">
 
 <head>
-    <!-- <title>
+    <title>
         <?= $tittle; ?>
-    </title> -->
-    <title>:: Iconic :: Home</title>
+    </title>
+    <!-- <title>:: Iconic :: Home</title> -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="Iconic Bootstrap 4.5.0 Admin Template">
